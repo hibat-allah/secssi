97c11556fca5bb3dc65a113c25baad7028a28c4f
hibat <hibatallah.nibou@etu.usthb.dz>
2023-01-04 14:09:51 +0100

    sql injection

--- Pages/login.php
+++ Pages/login.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="Style/home.css" rel="stylesheet">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="Style/home.css" rel="stylesheet">
     <title>Login</title>
 </head>
 <body>
@@ -23,63 +21,83 @@
     
     <img src="images/gradient.svg"  class="gradient3">
     <img src="images/gradient.svg"  class="gradient4">
+    
     <form action="/secssi/Pages/login.php " method="get" class="form">
               
-        <div class="container">
-          <label for="uname"><b>Username</b></label>
-          <input type="text" name="user" placeholder="Enter Username"  required>
+              <div class="container">
+                <label for="uname"><b>Username</b></label>
+                <input type="text" name="user" placeholder="Enter Username">
+            
+                <label for="psw"><b>Password</b></label>
+                <input type="password" name="pwd" placeholder="Enter Password">
+                    
+                <button type="submit" class="botton" id="button" name = "Submit" value="Submit">Login</button>
       
-          <label for="psw"><b>Password</b></label>
-          <input type="password" name="pwd" placeholder="Enter Password"  required>
-              
-          <button type="submit" class="botton" id="button" value="Submit">Login</button>
-
-        </div>
+              </div>
+            
+              <div class="container">
+                <button type="button" class="cancelbtn">Cancel</button>
+              </div>
       
-        <div class="container">
-          <button type="button" class="cancelbtn">Cancel</button>
-        </div>
+          </form>
+<?php
+// connect to data base:
+$user="root";
+$pwd="";
+$db="test";
+$chain_connex="mysql:host=localhost;dbname=$db";
+$id=new PDO($chain_connex,$user,$pwd);
+if($id == FALSE) echo"acces echoue";
+// -------------------------------------------------//
 
-    </form>
-    <?php
-    echo '$_GET["user"] $_GET["pwd"]';
-    if(isset($_GET["user"])&&!empty($_GET["pwd"])){
-    $x=1;
-    $nom=$_GET["user"];
-    $mdp=$_GET["pwd"];
-    $user="root";
-    $pwd="";
-    $db="test";
-    $chain_connex="mysql:host=localhost;dbname=$db";
-    $id=new PDO($chain_connex,$user,$pwd);
-    if($id) echo"acces reussit";
-    else echo"acces echoue";
-    $t="users";
-    $r="select `username`,`password` from `$t`";
-     $req=$id->prepare($r);
-    $e=$req->execute();
-    if($e) echo"<p style='color:#FFFFFF;'> succes select</p>";
-    else echo "<p style='color:#FFFFFF;'> erreur select</p>";
-    $enreg=$req->fetch();
-    $h=0;
-    while($enreg){
-      $A=$enreg["username"];
-      $B=$enreg["password"];
-      if ($A==$nom && $B==$mdp ){
-        $h=1;
-        if($A=='admin'){
-          header("Location: /secssi/Pages/admin.html");
-        }
-        else{
-          header("Location: /secssi/Pages/menu.html");
-          echo" identification acomplie avec succes</br>";
-        }
-      // echo'<a href="menu.php" style=" color: #FFFFFF;">Acceder au menu</a></br>';
-      // echo'<a href="motdp.php" style=" color: #FFFFFF;"> changer de mot de passe</a>';
+// fetch data:
+if(isset($_GET["Submit"])){
+$nom=$_GET["user"];
+$mdp=$_GET["pwd"];
+
+$requete = "SELECT * FROM users WHERE username='$nom' AND password='$mdp'"; //la requete SQL
+$req=$id->prepare($requete);
+$e=$req->execute(); // execution de la requete
+if($e){
+
+  $enreg=$req->fetch(); // fetching data
+  $number_Of_Rows = $req->rowCount();
+
+  if ($number_Of_Rows == 0){ //if username or password not found
+    echo" username ou mot de passe invalid";
+
+  }elseif($number_Of_Rows == 1){ //if one user is found
+    $A=$enreg["username"];
+	  $B=$enreg["password"];
+
+	  if ($A==$nom && $B==$mdp ){
+	  $h=1;
+	  if($A=='admin'){
+      header("Location: /secssi/Pages/admin.html");
     }
+    else{
+      header("Location: /secssi/Pages/menu.html");
+      echo" identification acomplie avec succes</br>";
+    }
+  }
+    else{
+      echo"<div>
+    <p> Email : ".$enreg["username"]." Password : ".$enreg["password"]."</p>
+    </div>";
+    }
+  }elseif($number_Of_Rows > 1){ //if multiple users are found (sql injection)
+    while($enreg){
+      echo"<div>
+      <p> Email : ".$enreg["username"]." Password : ".$enreg["password"]."</p>
+      </div>";
       $enreg=$req->fetch();
     }
-    }
-    ?>
+  }
+}
+else echo " erreur select";
+
+
+}
+?>
 </body>
 </html>
